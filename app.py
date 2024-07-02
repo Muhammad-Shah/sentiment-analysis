d1d14543670a672b9aa55ddee100324a7fc68af6
Muhammad-Shah <msh87376@gmail.com>
2024-07-02 21:18:25 +0500

    app updated

--- app.py
+++ app.py
@@ -18,18 +18,23 @@ model = load_model('sentiment.h5')
 with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
+
 def remove_html(text):
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
+
 
 def remove_url(text):
     url = re.compile(r'https?://\S+|www\.\S+')
     return url.sub(r'', text)
 
+
 def remove_punctuations(text):
     punctuations = string.punctuation
-    no_punctuations = "".join([char for char in text if char not in punctuations])
+    no_punctuations = "".join(
+        [char for char in text if char not in punctuations])
     return no_punctuations
+
 
 def chat_conversation(text):
     new_text = []
@@ -41,6 +46,8 @@ def chat_conversation(text):
     return " ".join(new_text)
 
 # Preprocess user input
+
+
 def preprocess(text):
     text = chat_conversation(text)
     text = text.lower()
@@ -49,14 +56,17 @@ def preprocess(text):
     text = remove_punctuations(text)
     return text
 
+
 def prepare_input(text):
     preprocessed_text = preprocess(text)
     sequences = tokenizer.texts_to_sequences([preprocessed_text])
     padded_sequences = pad_sequences(sequences, maxlen=200)
     return padded_sequences
 
+
 # Define Streamlit app
 st.title('Sentiment Analysis App')
+
 
 def main():
     """
@@ -72,7 +82,8 @@ def main():
     """
 
     # User input
-    text_input = st.text_input('Enter a sentence:', placeholder='Movie was fantastic!')
+    text_input = st.text_input(
+        'Enter a sentence:', placeholder='Movie was fantastic!')
 
     # Analyze sentiment
     if st.button('Analyze'):
@@ -82,7 +93,10 @@ def main():
             # Get the prediction probability and class
             pred_prob = output[0][0]
             sentiment = LABELS[int(pred_prob > 0.5)]
-            st.write(f'The review is: {sentiment} with {pred_prob:.2f} confidence.')
+            # st.write(f'The review is: {sentiment} with {pred_prob:.2f} confidence.')
+            st.write(
+                f'The review is: {sentiment} with a probability of {int(pred_prob.max().item() * 100)}% confidence.')
+
 
 if __name__ == '__main__':
     main()
